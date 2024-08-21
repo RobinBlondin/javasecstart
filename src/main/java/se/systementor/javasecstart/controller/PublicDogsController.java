a16366442ad24292519082719f51fb5c1c0d75a0
tarretarre <143603581+tarretarre@users.noreply.github.com>
2024-08-21 22:18:06 +0200

    Adding sorting and search in publicdogs

--- src/main/java/se/systementor/javasecstart/controller/PublicDogsController.java
+++ src/main/java/se/systementor/javasecstart/controller/PublicDogsController.java
@@ -1,6 +1,7 @@
 package se.systementor.javasecstart.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -10,27 +11,40 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import se.systementor.javasecstart.model.Dog;
 import se.systementor.javasecstart.model.DogRepository;
-import se.systementor.javasecstart.services.DogService;
-
-import java.util.List;
 
 @Controller
 public class PublicDogsController {
 
     @Autowired
-    private DogService dogService;
-    @Autowired
     private DogRepository dogRepository;
 
-
     @GetMapping(path="/dogs")
-    String list(Model model){
-        model.addAttribute("activeFunction", "publicdogs");
+    String list(Model model,
+                @RequestParam(defaultValue = "1") int pageNo,
+                @RequestParam(defaultValue = "10") int pageSize,
+                @RequestParam(defaultValue = "name") String sortCol,
+                @RequestParam(defaultValue = "ASC") String sortOrder,
+                @RequestParam(defaultValue = "") String searchTerm
+    ) {
+//        model.addAttribute("activeFunction", "home");
 //        setupVersion(model);
+        searchTerm = searchTerm.trim();
+        model.addAttribute("searchTerm", searchTerm);
 
-        model.addAttribute("dogs", dogService.getPublicDogs());
-        return "dogs";
+        Sort sort = Sort.by(Sort.Direction.fromString(sortOrder), sortCol);
+        Pageable pageable = PageRequest.of(pageNo-1, pageSize, sort);
+
+        Page<Dog> dogPage;
+        if (!searchTerm.isEmpty()) {
+            dogPage = dogRepository.findAllByNameContainsOrBreedContains(searchTerm, searchTerm, pageable);
+        } else {
+            dogPage = dogRepository.findAll(pageable);
+        }
+
+//        model.addAttribute("dogs", dogService.getPublicDogs());
+        model.addAttribute("dogs", dogPage.getContent());
+        model.addAttribute("totalPages", dogPage.getTotalPages());
+        model.addAttribute("pageNo", pageNo);
+        return "/dogs";
     }
-
-
 }
