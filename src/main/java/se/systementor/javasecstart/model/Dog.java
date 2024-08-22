b239669922ddf4c90db8c235803922801323f163
Michael Speiner <speiner.m@gmail.com>
2024-08-22 17:00:25 +0200

    Fixed entietie for dogs model so backend is protected from faulty registration

--- src/main/java/se/systementor/javasecstart/model/Dog.java
+++ src/main/java/se/systementor/javasecstart/model/Dog.java
@@ -1,6 +1,7 @@
 package se.systementor.javasecstart.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 
 import java.util.UUID;
 
@@ -12,29 +13,36 @@ public class Dog {
     @Column(name="Id")
     private UUID id;
 
+    @Pattern(regexp="[a-zA-Z]{1,30}", message="Age must be only letters and 1-30 characters long")
     @Column(name="Age")
     private String age;
 
+    @Pattern(regexp="[a-zA-Z]{1,30}", message="Gender must be only letters and 1-30 characters long")
     @Column(name="Gender")
     private String gender;
 
+    @Pattern(regexp="[a-zA-Z]{1,30}", message="Breed must be only letters and 1-30 characters long")
     @Column(name = "Breed")
     private String breed;
 
+    @Pattern(regexp="[a-zA-Z\\s-]{1,30}", message="SoldTo must be only letters, spaces, or dashes and 1-30 characters long")
     @Column(name="SoldTo")
     private String soldTo;
 
-
+    @Min(value = 1, message = "Price must be at least 1 digit long")
+    @Digits(integer=100, fraction=0, message="Price must be a number between 1 and 100 digits long")
     @Column(name="Price")
     private int price;
 
+    @Pattern(regexp="[a-zA-Z]{1,30}", message="Name must be only letters and 1-30 characters long")
     @Column(name="Name")
     private String name;
 
+    @Pattern(regexp="[a-zA-Z]{1,30}", message="Size must be only letters and 1-30 characters long")
     @Column(name="Size")
     private String size;
 
-
+    @Size(min = 1, max = 10000, message = "Image must be between 1 and 10000 characters long")
     @Column(name="Image")
     private String image;
 
