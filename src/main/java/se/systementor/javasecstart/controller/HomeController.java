cf9ad60f5bfd19c15d213772e9af5bf787f83815
Robin Blondin <robin.blondin@protonmail.com>
2024-08-25 19:41:25 +0200

    Moved auth logic to own controller

--- src/main/java/se/systementor/javasecstart/controller/HomeController.java
+++ src/main/java/se/systementor/javasecstart/controller/HomeController.java
@@ -11,43 +11,11 @@ import se.systementor.javasecstart.model.DogRepository;
 
 @Controller
 public class HomeController {
-    @Autowired
-    private DogRepository dogRepository;
-
-    UserService userService = new UserService();
-
     @GetMapping(path="/")
-    String empty(Model model)
-    {
+    String empty(Model model) {
         model.addAttribute("activeFunction", "home");
-//        setupVersion(model);
         return "home";
     }
-
-    @GetMapping("/logins")
-    String logins(Model model){
-        return "logins";
-    }
-
-    @PostMapping("/logincheck")
-    String logins(Model model, @RequestParam("username") String username, @RequestParam("password") String password){
-        System.out.println("Utanför");
-        try {
-            if (!userService.checkValidateUser(username, password)) {
-                System.out.println("Fel användare");
-                return "redirect:/logins?error";
-            } else {
-                return "redirect:/home";
-            }
-        }
-        catch (Exception e) {
-            return "redirect:/logins?userNotFound";
-        }
-
-    }
-
-
-
 }
 
 
